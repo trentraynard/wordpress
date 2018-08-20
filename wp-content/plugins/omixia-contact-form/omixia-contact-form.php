56154b1b2eadfd8b56eff975976d0351a1ad0485
trentraynard <trentersr@gmail.com>
2018-08-19 21:35:29 -0700

    Basic functionality added. Testing and will be adding updates

--- wp-content/plugins/omixia-contact-form/omixia-contact-form.php
+++ wp-content/plugins/omixia-contact-form/omixia-contact-form.php
@@ -13,3 +13,98 @@ Author URI: http://
 License: GPLv2 or later
 Text Domain: omixia-plugin
 */
+
+
+/*
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+if ( !defined( "ABSPATH" ) ) {
+
+	die;
+
+}
+
+//
+// Form the field and escape the incoming POST data
+//
+function html_form_code() {
+    echo '<form action="' . esc_url( $_SERVER['REQUEST_URI'] ) . '" method="post">';
+    echo '<p>';
+    echo 'Your Name (required) <br />';
+    echo '<input type="text" name="cf-name" pattern="[a-zA-Z0-9 ]+" value="' . ( isset( $_POST["cf-name"] ) ? esc_attr( $_POST["cf-name"] ) : '' ) . '" size="40" />';
+    echo '</p>';
+    echo '<p>';
+    echo 'Your Email (required) <br />';
+    echo '<input type="email" name="cf-email" value="' . ( isset( $_POST["cf-email"] ) ? esc_attr( $_POST["cf-email"] ) : '' ) . '" size="40" />';
+    echo '</p>';
+    echo '<p>';
+    echo 'Subject (required) <br />';
+    echo '<input type="text" name="cf-subject" pattern="[a-zA-Z ]+" value="' . ( isset( $_POST["cf-subject"] ) ? esc_attr( $_POST["cf-subject"] ) : '' ) . '" size="40" />';
+    echo '</p>';
+    echo '<p>';
+    echo 'Your Message (required) <br />';
+    echo '<textarea rows="10" cols="35" name="cf-message">' . ( isset( $_POST["cf-message"] ) ? esc_attr( $_POST["cf-message"] ) : '' ) . '</textarea>';
+    echo '</p>';
+    echo '<p><input type="submit" name="cf-submitted" value="Send"/></p>';
+    echo '</form>';
+}
+
+
+//
+//	Send out email on success
+//
+function deliver_mail() {
+
+    // if the submit button is clicked, send the email
+    if ( isset( $_POST['cf-submitted'] ) ) {
+
+        // sanitize form values
+        $name    = sanitize_text_field( $_POST["cf-name"] );
+        $email   = sanitize_email( $_POST["cf-email"] );
+        $subject = sanitize_text_field( $_POST["cf-subject"] );
+        $message = esc_textarea( $_POST["cf-message"] );
+
+        // get the blog administrator's email address
+        $to = get_option( 'admin_email' );
+
+        $headers = "From: $name <$email>" . "\r\n";
+
+        // If email has been process for sending, display a success message
+        if ( wp_mail( $to, $subject, $message, $headers ) ) {
+            echo '<div>';
+            echo '<p>Thanks for contacting me, expect a response soon.</p>';
+            echo '</div>';
+        } else {
+            echo 'An unexpected error occurred';
+        }
+    }
+}
+
+
+//
+//	Call back funcion
+//
+function cf_shortcode() {
+    ob_start();
+    deliver_mail();
+    html_form_code();
+
+    return ob_get_clean();
+}
+
+
+add_shortcode( 'sitepoint_contact_form', 'cf_shortcode' );
+
+?>
